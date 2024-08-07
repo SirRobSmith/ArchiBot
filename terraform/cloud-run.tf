cd31c7d3d4482aa4069096e2c3b06ea05f44d7e0
Rob Smith <robsmith@binaryrage.com>
2024-08-07 11:30:04 +0100

    no message

--- terraform/cloud-run.tf
+++ terraform/cloud-run.tf
@@ -84,9 +84,6 @@ resource "google_cloud_run_service" "default" {
   }
 }
 
-
-
-
 data "google_iam_policy" "noauth" {
   binding {
     role = "roles/run.invoker"
@@ -113,7 +110,42 @@ resource "google_sql_database_instance" "instance" {
 
   settings {
     tier = "db-f1-micro"
+    
+    ip_configuration {
+      ipv4_enabled                                  = false
+      private_network                               = google_compute_network.private_network.self_link
+      enable_private_path_for_google_cloud_services = true
+    }
+    
   }
 
   deletion_protection  = "true"
+
+}
+
+resource "google_compute_network" "private_network" {
+
+  name = "${var.service_name}-network"
+  
+}
+
+resource "google_compute_global_address" "private_ip_address" {
+
+  name          = "sql-private-ip-address"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = google_compute_network.private_network.id
+
+}
+
+resource "google_service_networking_connection" "private_vpc_connection" {
+
+  network                 = google_compute_network.private_network.id
+  service                 = "servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_address.name]
+}
+
+resource "random_id" "db_name_suffix" {
+  byte_length = 4
 }
