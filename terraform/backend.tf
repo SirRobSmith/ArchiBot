4fb98727558959c54bec20ef22789f0419b68feb
Rob Smith <robsmith@binaryrage.com>
2024-12-03 17:11:47 +0000

    no message

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,15 +1,4 @@
-variable "state_bucket" {
-  type = string
-  description = "The name of the bucket holding the Terraform state"
-}
-variable service_name {
-  type = string
-  description = "The given name of the service. "
-}
-
 terraform {
  backend "gcs" {
-   bucket  = var.state_bucket
-   prefix  = "${ var.service_name }/terraform/state"
  }
 }
